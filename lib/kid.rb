7be26594b3a8ea97916e8e80c927c582f0460871
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-14 01:40:08 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,5 +2,12 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
+  attr_accessor :name
+  include Dance
+  
+  
+  def initialize(name)
+    @name = name
+  end
   
 end
